d772045139a9e4a4f10d7fadf8e24d7486501770
Christopher Asuncion <christopherasuncion@Christophers-MacBook-Pro.local>
2025-04-19 11:14:55 +0800

    Finished practicum functionality

--- index.js
+++ index.js
@@ -1,71 +1,57 @@
+require('dotenv').config();
+
 const express = require('express');
 const axios = require('axios');
+const path = require('path');
+
 const app = express();
+const PORT = process.env.PORT || 3000;
+const PRIVATE_APP_ACCESS = process.env.HUBSPOT_API_KEY;
 
 app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+app.get('/', async (req, res) => {
+  const url = 'https://api.hubapi.com/crm/v3/objects/pets?properties=name,type,breed';
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  };
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    const response = await axios.get(url, { headers });
+    const pets = response.data.results;
+    res.render('homepage', { title: 'Pet List | Practicum', pets });
+  } catch (error) {
+    console.error('Error fetching pets:', error.response?.data || error.message);
+    res.status(500).send('Failed to load pets.');
+  }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
+app.get('/update-cobj', (req, res) => {
+  res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
 });
-*/
 
+app.post('/update-cobj', async (req, res) => {
+  const { name, type, breed } = req.body;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  };
+  const url = 'https://api.hubapi.com/crm/v3/objects/pets';
 
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+  try {
+    await axios.post(url, { properties: { name, type, breed } }, { headers });
+    res.redirect('/');
+  } catch (error) {
+    console.error('Error creating pet:', error.response?.data || error.message);
+    res.status(500).send('Failed to create pet.');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
